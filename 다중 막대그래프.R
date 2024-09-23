d1bb5d34a12b0feef9cc899bc3f381e543ff5694
OhJin-Soo <162969776+OhJin-Soo@users.noreply.github.com>
2024-09-23 19:45:04 +0900

    다중 막대그래프.R

--- 다중 막대그래프.R
+++ 다중 막대그래프.R
@@ -1,3 +1,4 @@
+# - barplot()에서는 “legend=” 인수를 사용하여 범례를 표시 할 수 있음
 data <- matrix(c(5,9,10,3,5,7,3,4),4,2) # 행렬 자료구조
 barplot(data)
 barplot(data, beside=T, main="학생수", legend=c("1학년", "2학년", "2학년", "3학년"))
